f9d1c02a7f17243f931b20d77e6f1383cdeed9f5
DanYu12 <dan.daniel.yu@gmail.com>
2023-08-16 12:12:29 -0400

    added bold font

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,13 +6,18 @@ const LandingPage = () => {
   return (
     <div className="font-misterpixel">
       <div className="text-[4rem]">Landing Page</div>
-      <Feliz>Hi! Welcome to the BostonHacks Land! Let me walk you through as you scroll!</Feliz>
+      <Feliz>
+        Hi! Welcome to the BostonHacks Land! Let me walk you through as you
+        scroll!
+      </Feliz>
       <ThemeBanner />
-      <Feliz>We have <span className="font-extrabold">three</span> special tracks this year waiting for you and your team to explore!</Feliz>
+      <Feliz>
+        We have <span className="font-ft88b">three</span> special tracks this
+        year waiting for you and your team to explore!
+      </Feliz>
       <div>
         <FAQ />
       </div>
-
     </div>
   );
 };
