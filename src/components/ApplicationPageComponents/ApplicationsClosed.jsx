e081f1e192f68bf868f45b6f374d3e9f25964c7f
DanYu12 <dan.daniel.yu@gmail.com>
2023-10-29 15:33:51 -0400

    closed apps

---
+++ src/components/ApplicationPageComponents/ApplicationsClosed.jsx
@@ -0,0 +1,13 @@
+const ApplicationsClosed = () => {
+  return (
+    <div className="h-[100vh] w-[100vw] flex flex-col items-center justify-center font-minecraft">
+      <h2 className="mb-10 text-3xl">Applications Have Closed!</h2>
+      <p className="font-xl">
+        Thanks for your interest in Bostonhacks! If you have any questions
+        please reach out to us at contact@bostonhacks.io!
+      </p>
+    </div>
+  );
+};
+
+export default ApplicationsClosed;
